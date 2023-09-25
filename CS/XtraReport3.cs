c1c3da7033df12c9efb2c107d28c92770584a529
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-26 00:45:31 +0400

    Change example status to out of maintenance

--- CS/XtraReport3.cs
+++
@@ -1,7 +0,0 @@
-namespace TransposeXRTable {
-    public partial class XtraReport3 : DevExpress.XtraReports.UI.XtraReport {
-        public XtraReport3() {
-            InitializeComponent();
-        }
-    }
-}
--- VB/XtraReport3.vb
+++
@@ -1,9 +0,0 @@
-﻿Namespace TransposeXRTable
-    Partial Public Class XtraReport3
-        Inherits DevExpress.XtraReports.UI.XtraReport
-
-        Public Sub New()
-            InitializeComponent()
-        End Sub
-    End Class
-End Namespace
